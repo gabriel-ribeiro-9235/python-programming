40044f2e4eac8e8daf7b6d330622d342ce325db8
gabriel-ribeiro-9235 <ribeiro.melo@aluno.ifsp.edu.br>
2025-11-30 17:45:37 -0300

    refactor(data-structure/semana12/ex05/main.py): move file handling from mais6notas to abasteceDic

--- data-structure/semana12/ex05/main.py
+++ data-structure/semana12/ex05/main.py
@@ -1,12 +1,19 @@
-def mais6notas(caminho):
+def abasteceDic(caminho):
     arq = open(caminho, "r")
-    lista = []
+    alunos = {}
     for linha in arq:
         linha = linha[:-1]
-        nome, notas = linha.split("\t")[0], linha.split("\t")[1:]
-        if len(notas) > 6:
-            lista.append(nome)
-    arq.close()
+        key, notas = linha.split("\t")[0], linha.split("\t")[1:]
+        alunos[key] = notas
+    return alunos
+
+
+def mais6notas(caminho):
+    dic = abasteceDic(caminho)
+    lista = []
+    for key in dic:
+        if len(dic[key]) > 6:
+            lista.append(key)
     return lista
 
 
