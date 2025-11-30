96b55cabfba2d87246a10f53261a3f0ba9af4079
gabriel-ribeiro-9235 <ribeiro.melo@aluno.ifsp.edu.br>
2025-11-30 18:22:34 -0300

    feat(data-structure/semana12/ex05/main.py): create minMax that returns each student's highest and lowest grade

--- data-structure/semana12/ex05/main.py
+++ data-structure/semana12/ex05/main.py
@@ -28,9 +28,24 @@ def calculaMedia(caminho):
         media = round((soma_notas / qnt), 2)
         medias[aluno] = media
     return medias
-    
+
+
+def minMax(caminho):
+    alunos = abasteceDic(caminho)
+    novo = {}
+    for key, val in alunos.items():
+        menor = maior = float(val[0])
+        for nota in val:
+            nota = float(nota)
+            if nota > maior:
+                maior = nota
+            if nota < menor:
+                menor = nota
+        novo[key] = [menor, maior]
+    return novo
 
 
 path = "data-structure/semana12/ex05/estudantes.dat"
 mais6 = mais6notas(path)
 med = calculaMedia(path)
+maior_menor_nota = minMax(path)
